dfb10c7ac29dec2a97d8ddcffe31b269290be567
tirol30 <awaya529@yahoo.co.jp>
2024-04-14 20:19:31 +0900

    update

---
+++ src/docfx-test-sln-ConsoleApp/test_dir1/Class1_2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace docfx_test_sln_ConsoleApp
+{
+    /// <summary>
+    /// テスト用のクラス2
+    /// </summary>
+    internal class Class1_2 : IClass1_2
+    {
+        private int var1 = 0;
+        /// <summary>
+        /// 整数を与えられると4倍にして返す
+        /// > [!NOTE]
+        /// > なにか有用なメモ
+        /// 
+        /// $$\left( \sum_{k=1}^n a_k b_k \right)^2 \leq \left( \sum_{k=1}^n a_k^2 \right) \left( \sum_{k=1}^n b_k^2 \right)$$
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int TestMethod2(int n)
+        {
+            return n * 4;
+        }
+    }
+}
---
+++ src/docfx-test-sln-ConsoleApp/test_dir1/IClass1_2.cs
@@ -0,0 +1,15 @@
+﻿namespace docfx_test_sln_ConsoleApp
+{
+    /// <summary>
+    /// テスト用のクラス2のインターフェース
+    /// </summary>
+    internal interface IClass1_2
+    {
+        /// <summary>
+        /// 整数を与えられると4倍にして返す
+        /// </summary>
+        /// <param name="n">入力される整数</param>
+        /// <returns>4倍にされた整数</returns>
+        int TestMethod2(int n);
+    }
+}
