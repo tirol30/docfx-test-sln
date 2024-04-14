c4902f97e78cf6da804fc0411e69281e1a117317
tirol30 <awaya529@yahoo.co.jp>
2024-04-14 16:49:06 +0900

    docfxフォルダを作成

---
+++ src/docfx-test-sln-ConsoleApp/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace docfx_test_sln_ConsoleApp
+{
+    /// <summary>
+    /// テスト用のクラス
+    /// </summary>
+    public class Class1
+    {
+        /// <summary>
+        /// 整数を与えられると２倍にして返す
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int TestMethod(int n)
+        {
+            return n * 2;
+        }
+    }
+}
